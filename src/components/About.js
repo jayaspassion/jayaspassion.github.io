02c3101ddfacb30abad0c267dbbe8a0044ac08b0
Jayasoorya <jayaspassion@gmail.com>
2021-11-05 17:58:50 +0530

    initial

---
+++ src/components/About.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+
+
+class About extends Component {
+  render() {
+    
+      // var profilepic = "images/" + this.props.resumeBasicInfo.image;
+    
+    if (this.props.resumeBasicInfo) {
+      var profilepic = "images/" + this.props.resumeBasicInfo.image;
+      var sectionName = this.props.resumeBasicInfo.section_name.about;
+      var hello = this.props.resumeBasicInfo.description_header;
+      var about1 = this.props.resumeBasicInfo.description;
+      var about2 = this.props.resumeBasicInfo.philosophy;
+      var about3 = this.props.resumeBasicInfo.preference;
+    }
+
+    return (
+      <section id="about">
+        <div className="col-md-12">
+          <h1 style={{ color: "black" }}>
+            <span>{sectionName}</span>
+          </h1>
+          <div className="row center mx-auto mb-5">
+            <div className="col-md-4 mb-5 center">
+              <div className="polaroid">
+                <span style={{ cursor: "auto" }}>
+                  <img
+                    height="250px"
+                    src={profilepic}
+                    alt="Avatar placeholder"
+                  />
+                  {/* <Icon
+                    icon={angularIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                  <Icon
+                    icon={reactIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                  <Icon
+                    icon={vueIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  /> */}
+                  <br/>
+                  <br/>
+                  <a href="https://drive.google.com/uc?export=download&id=14IditQRDLICAzEMlKu7xtnMrXaQkVh-G">Download Resume</a>
+                  </span>
+              </div>
+            </div>
+
+            <div className="col-md-8 center">
+              <div className="col-md-10">
+                <div className="card">
+                  
+                  <div
+                    className="card-body font-trebuchet text-justify ml-3 mr-3"
+                    style={{
+                      height: "auto",
+                      fontSize: "132%",
+                      lineHeight: "200%",
+                    }}
+                  >
+                    <br />
+                    <span className="wave">{hello}</span>
+                    <br />
+                    <br />
+                    {about1}
+                    <br />
+                    <br />
+                    {about2}
+                    <br />
+                    <br />
+                    {about3}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,104 @@
+// import React, { Component } from "react";
+// import Typical from "react-typical";
+
+
+// class Header extends Component {
+//   titles = [];
+
+//   constructor() {
+//     super();
+//     this.state = { checked: false };
+//     this.onThemeSwitchChange = this.onThemeSwitchChange.bind(this);
+//   }
+
+//   onThemeSwitchChange(checked) {
+//     this.setState({ checked });
+//     this.setTheme();
+//   }
+
+//   setTheme() {
+//     var dataThemeAttribute = "data-theme";
+//     var body = document.body;
+//     var newTheme =
+//       body.getAttribute(dataThemeAttribute) === "dark" ? "light" : "dark";
+//     body.setAttribute(dataThemeAttribute, newTheme);
+//   }
+
+//   render() {
+//     if (this.props.sharedData) {
+//       var name = this.props.sharedData.name;
+//       this.titles = this.props.sharedData.titles.map(x => [ x.toUpperCase(), 1500 ] ).flat();
+//     }
+
+//     const HeaderTitleTypeAnimation = React.memo( () => {
+//       return <Typical className="title-styles" steps={this.titles} loop={50} />
+//     }, (props, prevProp) => true);
+
+//     return (
+//       <header id="home" style={{ height: window.innerHeight, display: 'block' }}>
+//         <div className="row aligner" style={{height: '100%'}}>
+//           <div className="col-md-12">
+//             <div>
+//               <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
+//               <br/>
+//               <h1 className="mb-0">
+//                 <Typical steps={[name]} wrapper="p" />
+//               </h1>
+//               <div className="title-container">
+//                 <HeaderTitleTypeAnimation />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Header;
+
+//**********Functional Component instead of class*********/
+
+import React from "react";
+import Typical from "react-typical";
+// import { useState } from "react";
+
+function Header(props) {
+  var titles = [];
+//   const [state, setState] = useState({ checked: false });
+
+  if (props.resumeData) {
+    var name = props.resumeData.name;
+    titles = props.resumeData.titles.map((x) => [x.toUpperCase(), 1500]).flat();
+  }
+
+  const HeaderTitleTypeAnimation = () => {
+    return <Typical className="title-styles" steps={titles} loop={50} />;
+  };
+
+  return (
+    <header id="home" style={{ height: window.innerHeight, display: "block" }}>
+      <div className="row aligner" style={{ height: "100%" }}>
+        <div className="col-md-12">
+          <div>
+            <span
+              className="iconify header-icon"
+              data-icon="la:laptop-code"
+              data-inline="false"
+            ></span>
+            <br />
+            <h1 className="mb-0">
+              <Typical className="text-white" steps={[name]} wrapper="p" />
+            </h1>
+            <div className="title-container text-white">
+              <HeaderTitleTypeAnimation />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
+
