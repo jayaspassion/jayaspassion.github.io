dac19c3ac2bb19a41a770655003d5d176ed74910
Jayasoorya <jayaspassion@gmail.com>
2021-11-05 19:29:20 +0530

    Updates

---
+++ precache-manifest.afd041030611d3536d46ee1606fc341c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399b24d8a2860c42f1bbe39da08779a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c3d49c3171e61dc3cfbe",
+    "url": "./static/css/2.0504b4c7.chunk.css"
+  },
+  {
+    "revision": "e6e5bbf0e32df7b14be5",
+    "url": "./static/css/main.e6b0eecd.chunk.css"
+  },
+  {
+    "revision": "c3d49c3171e61dc3cfbe",
+    "url": "./static/js/2.b3aa8103.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "./static/js/2.b3aa8103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e5bbf0e32df7b14be5",
+    "url": "./static/js/main.81539203.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
